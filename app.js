1bc4131dcb2667e031b168b403cc489d59cbaa3f
dookye <dookye@web.de>
2025-07-21 20:27:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ const diceConfig = {
         player1Score: 0,
         player2Score: 0,
         currentPlayer: 1,
-        totalRounds: 4, // wert auf 20 setzen, wenn jeder spieler 10 runden spielt
+        totalRounds: 2, // wert auf 20 setzen, wenn jeder spieler 10 runden spielt
         currentRound: 0,
         diceValue: 0,
         attemptsMade: 0,
