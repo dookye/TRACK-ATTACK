32608b48094337f5be909d71bcc41c71e69c38f0
dookye <dookye@web.de>
2025-07-28 19:07:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -116,16 +116,16 @@ document.addEventListener('DOMContentLoaded', () => {
     let lastGameScreenVisible = '';
 
     const playlists = {
-        'Pop Hits 200ß-2025': ['6mtYuOxzl58vSGnEDtZ9uB', '34NbomaTu7YuOYnky8nLXL'],
-        'Die grössten Hits aller Zeiten': ['2si7ChS6Y0hPBt4FsobXpg', '2y09fNnXHvoqc1WGHvbhkZ'],
-        'deutsch Songs von früher bis heute': ['7h64UGKHGWM5ucefn99frR', '4ytdW13RHl5u9dbRWAgxSZ'],
-        'Party Hits': ['53r5W67KJNIeHWAhVOWPDr'],
-        'Skate-Punk': ['7qGvinYjBfVpl1FJFkzGqV'],
-        'Deutsch-Punk': ['3sQLh9hYyJQZ0qWrtJG1OO', '4iR7Xq1wP9GRbGLm2qFBYw'],
-        'Top 100 One Hit Wonders': ['1t1iRfYh9is6FH6hvn58lt', '77IXl4Gh7AZLyVLx66NkqV'],
-        'Girl- and Boybands': ['11Q0O9t6MGGXrKFaeqRRwm'],
-        'deutsche Disney-Songs': ['6CdPoZsFja4LOrTYTvHrY5'],
-        'Lagerfeuer Klassiker': ['3TfJ6iMeqPXPLW8sxuQgcd']
+        'pop hits 200ß-2025': ['6mtYuOxzl58vSGnEDtZ9uB', '34NbomaTu7YuOYnky8nLXL'],
+        'die größten hits aller zeiten': ['2si7ChS6Y0hPBt4FsobXpg', '2y09fNnXHvoqc1WGHvbhkZ'],
+        'deutsch songs von früher bis heute': ['7h64UGKHGWM5ucefn99frR', '4ytdW13RHl5u9dbRWAgxSZ'],
+        'party hits': ['53r5W67KJNIeHWAhVOWPDr'],
+        'skate-punk': ['7qGvinYjBfVpl1FJFkzGqV'],
+        'deutsch-punk': ['3sQLh9hYyJQZ0qWrtJG1OO', '4iR7Xq1wP9GRbGLm2qFBYw'],
+        'top 100 one hit wonders': ['1t1iRfYh9is6FH6hvn58lt', '77IXl4Gh7AZLyVLx66NkqV'],
+        'girl- and boybands': ['11Q0O9t6MGGXrKFaeqRRwm'],
+        'deutsche disney-songs': ['6CdPoZsFja4LOrTYTvHrY5'],
+        'lagerfeuer klassiker': ['3TfJ6iMeqPXPLW8sxuQgcd']
     };
 
     //=======================================================================
